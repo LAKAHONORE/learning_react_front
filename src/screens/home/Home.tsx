ed957ceba0bf7b079c05e3f50828766455e4d31f
LAKAHONORE <lakahonore1@gmail.com>
2025-03-14 14:05:36 +0100

    feat:(home: ajout de la sction bibliothèque)

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,6 +1,7 @@
 import CategorieSection from "./categorie-section/CategorieSection";
 import FormationSection from "./formations-section/FormationSection";
 import Hero from "./components/hero/Hero";
+import BibliothequeSection from "./bibliotheque-section/BibliothequeSection";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <CategorieSection />
       <FormationSection />
+      <BibliothequeSection />
     </>
   )
 }
---
+++ src/screens/home/bibliotheque-section/BibliothequeSection.tsx
@@ -0,0 +1,12 @@
+import SectionTitle from "../components/section-title/SectionTitle";
+
+export default function BibliothequeSection() {
+    return (
+        <>
+            <SectionTitle title={`Notre bibliothèque`} />
+            <section className="flex flex-col w-full bg-red-500 h-16 my-16">
+
+            </section>
+        </>
+    )
+}
